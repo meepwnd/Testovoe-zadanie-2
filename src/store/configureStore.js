7ba5f9f2f3ed1d676cdb611b554ed3f005468243
meepwnd <32109262+meepwnd@users.noreply.github.com>
2019-04-16 17:15:26 +0300

    add composeWithDevtools

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,9 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
 import reducer from "../reducers";
 import ReduxThunk from "redux-thunk";
 
 export const store = createStore(
   reducer,
-  compose(
-    applyMiddleware(ReduxThunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  composeWithDevTools(applyMiddleware(ReduxThunk))
 );
